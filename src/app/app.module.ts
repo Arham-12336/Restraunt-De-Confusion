4e1fa4c9835d20bdd007146079b331b2cc719a80
Muhammad Arham <67412607+Arham-12336@users.noreply.github.com>
2022-10-20 17:22:52 +0500

    Angular header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,20 +12,22 @@ import { MatButtonModule } from '@angular/material';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    MatToolbarModule, 
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
