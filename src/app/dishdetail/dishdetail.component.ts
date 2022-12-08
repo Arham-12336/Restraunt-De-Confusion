8275b2ed2b64b292686d28b5b56632950f580c54
Muhammad Arham <67412607+Arham-12336@users.noreply.github.com>
2022-12-08 16:13:33 +0500

    Updated the dishdetail.ts file with onvalue chaneg obersvarbale

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,} from '@angular/core';
 import { Dish } from '../shared/dish';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import {switchMap} from 'rxjs/operators';
-
+import {FormBuilder , FormGroup , Validators} from '@angular/forms';
+import {Comment} from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -16,9 +17,29 @@ export class DishdetailComponent implements OnInit {
     dishIds: string[];
     prev:string;
     next:string;
+
+    commentForm: FormGroup;
+    comment: Comment;
+    formErrors = {
+      author: '' ,
+      rating: 5 ,
+      comment: ''
+    };
+    validationMessages = {
+      'author': {
+        'required': 'Author is required.' ,
+        'minlength': 'Author must be at least 2 characters long.'
+      } ,
+      'comment': {
+        'required': 'comment is required.' ,
+        'minlength': 'comment must be at least 2 characters long.'
+      } ,
+    };
   
 
-  constructor(private dishService: DishService,private route: ActivatedRoute, private location: Location) { }
+  constructor(private dishService: DishService,private route: ActivatedRoute, private location: Location,private fb: FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit() { //urls will be dishdetail 0
     //The activated route services provide a observable name params and what
@@ -45,6 +66,51 @@ export class DishdetailComponent implements OnInit {
   goback(): void{
     this.location.back();
     //This will help us in getiing back through using a function back
+  }
+  private createForm(): void {
+    this.commentForm = this.fb.group({
+      author: ['' , [Validators.required , Validators.minLength(2)]] ,
+      rating: 5 ,
+      comment: ['' , [Validators.required , Validators.minLength(2)]]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+      author: '' ,
+      rating: 5 ,
+      comment: ''
+    });
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
 }
